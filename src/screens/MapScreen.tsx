6a79f24a7f6bf8b7c35bc1b0062cb61435434672
Fernando Pachon <josep3194@gmail.com>
2022-12-20 22:15:26 -0500

    feat:init sesion 17

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -5,7 +5,14 @@ import { View,Text,StyleSheet } from 'react-native'
 export const MapScreen = () => {
   return (
 <View style={styles.container}>
-     <Text>MapScreen </Text>
+<MapView
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
 </View>
  )
 };
